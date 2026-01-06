a5da89fc5ec0c6d2f8d81010ef5ae6fdb25a0183
ZhuolinYang <80162521+ZhuolinYang@users.noreply.github.com>
2026-01-06 15:55:42 +0000

    LC-two-sum [Runtime: 1726 ms; Memory: 19.8 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
-                if i + j == target:
+                if nums[i] + nums[j] == target:
                     return [i, j]
         return []
         
