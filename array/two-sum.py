a539a4ece54c30f4c7ea2e3424f43c3f590a8bad
ZhuolinYang <80162521+ZhuolinYang@users.noreply.github.com>
2026-01-07 11:06:36 +0000

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.py
+++ array/two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]: 
         seen = {}
 
-        for i, num in numerate(nums):
+        for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
                 return [seen[complement], i]
